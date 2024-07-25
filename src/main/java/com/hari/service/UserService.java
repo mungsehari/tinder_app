a9645723e1f133d51e9a4e685d8924100d073300
mungsehari <mungse.hari@gmail.com>
2024-07-25 12:38:15 +0530

    create in UserService

---
+++ src/main/java/com/hari/service/UserService.java
@@ -0,0 +1,40 @@
+package com.hari.service;
+
+import com.hari.model.User;
+import com.hari.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String id, User userDetails) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setName(userDetails.getName());
+            user.setEmail(userDetails.getEmail());
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
